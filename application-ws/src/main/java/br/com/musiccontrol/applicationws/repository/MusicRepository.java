36b3c346c1fc9d950bbe254327c922c0e7ae7875
malevisa <matheus.vsantos@bandtec.com.br>
2023-03-27 11:10:25 -0300

    feat: Adding methods to return user repertory statistics

--- application-ws/src/main/java/br/com/musiccontrol/applicationws/repository/MusicRepository.java
+++ application-ws/src/main/java/br/com/musiccontrol/applicationws/repository/MusicRepository.java
@@ -1,7 +1,9 @@
 package br.com.musiccontrol.applicationws.repository;
 
 import br.com.musiccontrol.applicationws.domain.Music;
+import br.com.musiccontrol.applicationws.domain.StatisticsBasic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -15,4 +17,14 @@ public interface MusicRepository extends JpaRepository<Music, Long> {
     List<Music> findByIdUserIdUserAndMusicArtistStartingWith(Long idUser, String value);
 
     List<Music> findByIdUserIdUserAndMusicGenreStartingWith(Long idUser, String value);
+
+    @Query("SELECT m.musicGenre, count(m.musicGenre) as countValue FROM Music m WHERE m.idUser = ?1 group by m.musicGenre order by countValue")
+    List<StatisticsBasic> countMusicGenreByIdUser(Long idUser);
+
+    @Query("SELECT m.musicArtist, count(m.musicArtist) as countValue FROM Music m WHERE m.idUser = ?1 group by m.musicArtist order by countValue")
+    List<StatisticsBasic> countMusicArtistByIdUser(Long idUser);
+
+    @Query("SELECT m.instrument, count(m.instrument) as countValue FROM Music m WHERE m.idUser = ?1 group by m.instrument order by countValue")
+    List<StatisticsBasic> countMusicInstrumentByIdUser(Long idUser);
+
 }
