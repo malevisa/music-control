605b50e3f0045712e6463e776452e9085be84502
malevisa <matheus.vsantos@bandtec.com.br>
2023-04-03 00:50:22 -0300

    feat: Adding bean validation annotations

--- application-ws/src/main/java/br/com/musiccontrol/applicationws/controller/dto/request/LoginRequestDTO.java
+++ application-ws/src/main/java/br/com/musiccontrol/applicationws/controller/dto/request/LoginRequestDTO.java
@@ -3,13 +3,16 @@ package br.com.musiccontrol.applicationws.controller.dto.request;
 //import jakarta.validation.constraints.NotBlank;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class LoginRequestDTO {
 
     @NotBlank(message = "Favor informar o login.")
+    @Size(min = 8, max = 40)
     private String login;
 
     @NotBlank(message = "Favor informar a senha.")
+    @Size(min = 8, max = 30)
     private String password;
 
     public LoginRequestDTO(String login, String password) {
@@ -21,8 +24,15 @@ public class LoginRequestDTO {
         return login;
     }
 
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
     public String getPassword() {
         return password;
     }
 
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
