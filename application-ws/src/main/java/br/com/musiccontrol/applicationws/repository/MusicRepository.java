c53689f264dba73c782168523cf45b51f68599a0
malevisa <matheus.vsantos@bandtec.com.br>
2023-04-03 01:00:31 -0300

    fix: Changing queries of MusicRepository methods

--- application-ws/src/main/java/br/com/musiccontrol/applicationws/repository/MusicRepository.java
+++ application-ws/src/main/java/br/com/musiccontrol/applicationws/repository/MusicRepository.java
@@ -2,6 +2,7 @@ package br.com.musiccontrol.applicationws.repository;
 
 import br.com.musiccontrol.applicationws.domain.Music;
 import br.com.musiccontrol.applicationws.domain.StatisticsBasic;
+import br.com.musiccontrol.applicationws.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -10,21 +11,47 @@ import java.util.List;
 public interface MusicRepository extends JpaRepository<Music, Long> {
     List<Music> findByIdUserIdUser(Long idUser);
 
-    List<Music> findByIdUserIdUserAndInstrumentStartingWith(Long idUser, String value);
+    List<Music> findByIdUserIdUserAndInstrumentStartingWithIgnoreCase(Long idUser, String value);
 
-    List<Music> findByIdUserIdUserAndMusicNameStartingWith(Long idUser, String value);
+    List<Music> findByIdUserIdUserAndMusicNameStartingWithIgnoreCase(Long idUser, String value);
 
-    List<Music> findByIdUserIdUserAndMusicArtistStartingWith(Long idUser, String value);
+    List<Music> findByIdUserIdUserAndMusicArtistStartingWithIgnoreCase(Long idUser, String value);
 
-    List<Music> findByIdUserIdUserAndMusicGenreStartingWith(Long idUser, String value);
+    List<Music> findByIdUserIdUserAndMusicGenreStartingWithIgnoreCase(Long idUser, String value);
 
-    @Query("SELECT m.musicGenre, count(m.musicGenre) as countValue FROM Music m WHERE m.idUser = ?1 group by m.musicGenre order by countValue")
-    List<StatisticsBasic> countMusicGenreByIdUser(Long idUser);
+    @Query("select " +
+            "new br.com.musiccontrol.applicationws.domain.StatisticsBasic(" +
+            "lower(m.musicGenre) as musicGenre," +
+            "count(m.musicGenre) as countValue" +
+            ") " +
+            "FROM Music m " +
+            "where m.idUser = ?1 " +
+            "group by musicGenre " +
+            "order by countValue desc"
+    )
+    List<StatisticsBasic> getCountMusicGenreByIdUser(User user);
 
-    @Query("SELECT m.musicArtist, count(m.musicArtist) as countValue FROM Music m WHERE m.idUser = ?1 group by m.musicArtist order by countValue")
-    List<StatisticsBasic> countMusicArtistByIdUser(Long idUser);
+    @Query("select " +
+            "new br.com.musiccontrol.applicationws.domain.StatisticsBasic(" +
+            "lower(m.musicArtist) as musicArtist," +
+            "count(m.musicArtist) as countValue" +
+            ") " +
+            "FROM Music m " +
+            "where m.idUser = ?1 " +
+            "group by musicArtist " +
+            "order by countValue desc"
+    )
+    List<StatisticsBasic> countMusicArtistByIdUser(User user);
 
-    @Query("SELECT m.instrument, count(m.instrument) as countValue FROM Music m WHERE m.idUser = ?1 group by m.instrument order by countValue")
-    List<StatisticsBasic> countMusicInstrumentByIdUser(Long idUser);
-
+    @Query("select " +
+            "new br.com.musiccontrol.applicationws.domain.StatisticsBasic(" +
+            "m.instrument," +
+            "count(m.instrument) as countValue" +
+            ") " +
+            "FROM Music m " +
+            "where m.idUser = ?1 " +
+            "group by m.instrument " +
+            "order by countValue desc"
+    )
+    List<StatisticsBasic> countMusicInstrumentByIdUser(User user);
 }
