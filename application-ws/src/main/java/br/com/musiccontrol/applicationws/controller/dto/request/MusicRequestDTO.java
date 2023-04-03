037deb92bc5f46462af1be7e91258657c2fe69dc
malevisa <matheus.vsantos@bandtec.com.br>
2023-04-03 00:51:09 -0300

    feat: Adding bean validation annotations

--- application-ws/src/main/java/br/com/musiccontrol/applicationws/controller/dto/request/MusicRequestDTO.java
+++ application-ws/src/main/java/br/com/musiccontrol/applicationws/controller/dto/request/MusicRequestDTO.java
@@ -1,11 +1,21 @@
 package br.com.musiccontrol.applicationws.controller.dto.request;
 
+import javax.validation.constraints.NotBlank;
+
 public class MusicRequestDTO {
 
     private Long idUser;
+
+    @NotBlank(message = "Nome da música obrigatório.")
     private String musicName;
+
+    @NotBlank(message = "Artista obrigatório.")
     private String musicArtist;
+
+    @NotBlank(message = "Gênero musical obrigatório.")
     private String musicGenre;
+
+    @NotBlank(message = "Instrumento obrigatório")
     private String instrument;
 
     public MusicRequestDTO(Long idUser, String musicName, String musicArtist, String musicGenre, String instrument) {
@@ -38,16 +48,31 @@ public class MusicRequestDTO {
         return musicName;
     }
 
+    public void setMusicName(String musicName) {
+        this.musicName = musicName;
+    }
+
     public String getMusicArtist() {
         return musicArtist;
+    }
+
+    public void setMusicArtist(String musicArtist) {
+        this.musicArtist = musicArtist;
     }
 
     public String getMusicGenre() {
         return musicGenre;
     }
 
+    public void setMusicGenre(String musicGenre) {
+        this.musicGenre = musicGenre;
+    }
+
     public String getInstrument() {
         return instrument;
     }
 
+    public void setInstrument(String instrument) {
+        this.instrument = instrument;
+    }
 }
