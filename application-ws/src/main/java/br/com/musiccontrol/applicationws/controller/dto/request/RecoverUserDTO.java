c9fa381263d46a36dbf00f31d289db300f61f3b0
malevisa <matheus.vsantos@bandtec.com.br>
2023-04-03 00:51:24 -0300

    feat: Adding bean validation annotations

--- application-ws/src/main/java/br/com/musiccontrol/applicationws/controller/dto/request/RecoverUserDTO.java
+++ application-ws/src/main/java/br/com/musiccontrol/applicationws/controller/dto/request/RecoverUserDTO.java
@@ -1,8 +1,16 @@
 package br.com.musiccontrol.applicationws.controller.dto.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class RecoverUserDTO {
 
+    @NotBlank(message = "Login obrigatório.")
+    @Size(min = 8, max = 40)
     private String login;
+
+    @NotBlank(message = "E-mail obrigatório.")
+    @Size(min = 8, max = 30)
     private String email;
 
     public RecoverUserDTO(String login, String email) {
