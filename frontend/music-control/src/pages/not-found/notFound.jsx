82f756c28bebe38e7a3ef52c112161908c077298
malevisa <matheus.vsantos@bandtec.com.br>
2023-04-14 00:05:14 -0300

    feat: Adding notFound page

---
+++ frontend/music-control/src/pages/not-found/notFound.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Navbar from "../../components/navbar/navbar";
+import Footer from "../../components/footer/footer";
+import ImgError from '../../assets/404-error.png';
+import '../not-found/notFound.css';
+
+function NotFound404() {
+
+    return (
+
+        <>
+
+            <Navbar />
+
+            <div className="notfound_content">
+                <h1>Desculpe, página não encontrada.</h1>
+                <img src={ImgError} alt="" />
+            </div>
+
+            <Footer />
+
+        </>
+
+    );
+
+}
+
+export default NotFound404;
