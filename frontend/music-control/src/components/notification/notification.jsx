0be3b794e40d69141b85c1ab510fab2a656c0bbd
malevisa <matheus.vsantos@bandtec.com.br>
2023-05-03 23:02:04 -0300

    feat: Adding new component notification

---
+++ frontend/music-control/src/components/notification/notification.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import '../notification/notification.css'
+
+export default function Notification(props) {
+
+    return (
+        <>
+            <div id="notification" className="notification show_notification">
+                <div>
+                    <div className={props.status ? 'content_notification success' : 'content_notification error'}>
+                        <div className="header">
+                            <h2 className="title_notification">{props.title} teste</h2>
+                            <h4 className="close_notification">X</h4>
+                        </div>
+                        <div className="notification_message">
+                            <span>
+                                {props.content} teste
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
