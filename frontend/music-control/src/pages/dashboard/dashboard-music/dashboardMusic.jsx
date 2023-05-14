dcff393ce7c7c49d893de43063f03c3dc037ef6f
malevisa <matheus.vsantos@bandtec.com.br>
2023-05-14 18:36:34 -0300

    feat: Adding filterMusic integration

--- frontend/music-control/src/pages/dashboard/dashboard-music/dashboardMusic.jsx
+++ frontend/music-control/src/pages/dashboard/dashboard-music/dashboardMusic.jsx
@@ -7,10 +7,13 @@ import ModalEditarMusica from "../../../components/modals-components/modal-edit-
 import { musicUri } from "../../../service/musicApi";
 import { useLocation } from "react-router-dom";
 import * as ReactDOM from 'react-dom/client';
+import Notification from "../../../components/notification/notification";
 
 function DashboardMusic() {
 
     const [musics, setMusics] = useState([]);
+    const [fieldComboBox, setFieldComboBox] = useState();
+    const [filterValue, setFilterValue] = useState();
 
     const location = useLocation();
 
@@ -28,6 +31,15 @@ function DashboardMusic() {
     useEffect(() => {
         getMusicsByUser()
     }, [location.key])
+
+    function closeNotification() {
+        const notification = document.querySelectorAll('.show_notification');
+
+        for (let index = 0; index < notification.length; index++) {
+            notification.item(index).classList.remove('show_notification');
+        }
+
+    }
 
     function initModal(modalId) {
         const modal = document.getElementById(modalId);
@@ -76,6 +88,48 @@ function DashboardMusic() {
         })
     }
 
+    function filterMusics(event) {
+        event.preventDefault();
+        musicUri.get(`/${sessionStorage.getItem('idUser')}/${fieldComboBox}/${filterValue}`).then((response) => {
+            setMusics(response.data.listMusics === undefined ? [] : response.data.listMusics);
+        }).catch((error) => {
+
+            const boxNotification = document.getElementById('box-notification');
+
+            const root = ReactDOM.createRoot(
+                boxNotification
+            );
+
+            const errors = error.response.data;
+
+            const elements = []
+
+            errors.length > 0 ? Array.from(errors, (error, index) => {
+
+                elements[index] = <Notification
+                    key={index}
+                    status={false}
+                    title={error.campo == null ? "Erro" : error.campo}
+                    content={error.message}
+                />
+
+            }) : elements[0] = <Notification
+                key={0}
+                status={false}
+                title={errors.campo == null ? "Erro" : errors.campo}
+                content={errors.message}
+            />
+
+            root.render(elements);
+
+            const interval = setInterval(() => {
+                closeNotification();
+                clearInterval(interval);
+            }, 1000 * 7);
+
+        })
+    }
+
     return (
         <>
 
@@ -89,18 +143,18 @@ function DashboardMusic() {
                 <div className="search_box">
                     <form>
                         <div className="box-input">
-                            <input required className="input" type="text" id="search" /> <label htmlFor="search">Pesquisar</label>
+                            <input onChange={(event) => setFilterValue(event.target.value)} required className="input" type="text" id="search" /> <label htmlFor="search">Pesquisar</label>
                         </div>
                         <div className="combo_field">
-                            <select required id="field">
-                                <option selected defaultValue={"invalidField"} disabled>Campo...</option>
+                            <select onChange={(event) => setFieldComboBox(event.target.value)} required id="field">
+                                <option selected value={"invalidField"} disabled>Campo...</option>
                                 <option value="musicName">Nome</option>
                                 <option value="musicArtist">Artista</option>
                                 <option value="musicGenre">Gênero Musical</option>
                                 <option value="instrument">Instrumento</option>
                             </select>
                         </div>
-                        <button className="btn_filter">Filtrar</button>
+                        <button onClick={(event) => filterMusics(event)} className="btn_filter">Filtrar</button>
                     </form>
                 </div>
                 <div className="box_table">
