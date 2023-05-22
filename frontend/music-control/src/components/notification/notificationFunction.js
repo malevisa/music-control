316d3db6df563abe67863e9cace017e87f91a044
malevisa <matheus.vsantos@bandtec.com.br>
2023-05-22 12:01:59 -0300

    feat: Adding generateNotification and renderNotification functions

--- frontend/music-control/src/components/notification/notificationFunction.js
+++ frontend/music-control/src/components/notification/notificationFunction.js
@@ -1,3 +1,49 @@
+import Notification from "./notification";
+import * as ReactDOM from 'react-dom/client';
+
+function generateNotification(boolean, data) {
+
+    const elements = []
+
+    if (boolean) {
+        elements[0] = <Notification
+            key={0}
+            status={boolean}
+            title={data.title}
+            content={data.content}
+        />
+    } else {
+        data.length > 0 ? Array.from(data, (error, index) => {
+
+            elements[index] = <Notification
+                key={index}
+                status={boolean}
+                title={error.campo == null ? "Erro" : error.campo}
+                content={error.message}
+            />
+
+        }) : elements[0] = <Notification
+            key={0}
+            status={boolean}
+            title={data.campo == null ? "Erro" : data.campo}
+            content={data.message}
+        />
+    }
+
+    renderNotification(elements);
+
+}
+
+function renderNotification(elements) {
+    const boxNotification = document.getElementById('box-notification');
+
+    const root = ReactDOM.createRoot(
+        boxNotification
+    );
+
+    root.render(elements);
+}
+
 function closeNotification() {
     const notification = document.querySelectorAll('.show_notification');
 
@@ -7,4 +53,4 @@ function closeNotification() {
 
 }
 
-export { closeNotification };
+export { closeNotification, generateNotification };
